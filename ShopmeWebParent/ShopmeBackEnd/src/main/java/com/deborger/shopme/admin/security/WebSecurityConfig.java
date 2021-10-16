a0972040b3819c1169c4f074d48ba93426699bf0
borgepl <pdeborger@gmail.com>
2021-10-16 13:40:20 +0200

    Custom Login page

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/deborger/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/deborger/shopme/admin/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.deborger.shopme.admin.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,7 +20,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // Permit All access to test the application (only for Testing purpose)
-        http.authorizeRequests().anyRequest().permitAll();
+        // http.authorizeRequests().anyRequest().permitAll();
 
+        // Permit All access to Authenticated users
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .usernameParameter("email")
+                    .permitAll();
+
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/images/**","/js/**","/webjars/**");
     }
 }
