ee77c9620ae6207f14a1e9c6e3f884b60902fd34
borgepl <pdeborger@gmail.com>
2021-11-05 16:01:49 +0100

    Refactor MvcConfig.java

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/deborger/shopme/admin/MvcConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/deborger/shopme/admin/MvcConfig.java
@@ -12,25 +12,19 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String dirNamePhotos = "user-photos";
-        Path userPhotosDir = Paths.get(dirNamePhotos);
-        String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
 
-        registry.addResourceHandler("/" + dirNamePhotos + "/**")
-                .addResourceLocations("file:/" + userPhotosPath + "/");
+        exposeDirectory(registry,"user-photos");
+        exposeDirectory(registry,"category-images");
+        exposeDirectory(registry,"brand-images");
 
-        String dirName = "category-images";
-        Path catImagesDir = Paths.get(dirName);
-        String catImagesPath = catImagesDir.toFile().getAbsolutePath();
+    }
+
+    private void exposeDirectory(ResourceHandlerRegistry registry, String dirName) {
+
+        Path path = Paths.get(dirName);
+        String absolutePath = path.toFile().getAbsolutePath();
 
         registry.addResourceHandler("/" + dirName + "/**")
-                .addResourceLocations("file:/" + catImagesPath + "/");
-
-        String dirNameBrands = "brand-images";
-        Path brandImagesDir = Paths.get(dirNameBrands);
-        String brandImagesPath = brandImagesDir.toFile().getAbsolutePath();
-
-        registry.addResourceHandler("/" + dirNameBrands + "/**")
-                .addResourceLocations("file:/" + brandImagesPath + "/");
+                .addResourceLocations("file:/" + absolutePath + "/");
     }
 }
