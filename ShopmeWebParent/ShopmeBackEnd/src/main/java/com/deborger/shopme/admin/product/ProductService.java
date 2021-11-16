97921d0caee671668ccca937b3a81cef8fb9d881
borgepl <pdeborger@gmail.com>
2021-11-16 14:33:13 +0100

    Product listing page code

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/deborger/shopme/admin/product/ProductService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/deborger/shopme/admin/product/ProductService.java
@@ -1,4 +1,18 @@
 package com.deborger.shopme.admin.product;
 
+import com.deborger.shopme.common.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> listAll() {
+        return (List<Product>) productRepository.findAll();
+    }
 }
